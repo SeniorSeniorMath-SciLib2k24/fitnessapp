2d8575744f85e797a07478c37fb2a648b08a6e5e
megantriplett <megantriplett10@gmail.com>
2024-10-14 18:17:19 -0400

    dumped all of my work so far - up until enum section

---
+++ fitness/src/App.tsx
@@ -0,0 +1,30 @@
+
+// import './App.css';
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
+//does exist in the actual build
+// enum SelectedPage {
+//   Home = 'home',
+//   Benefits = 'benefits',
+//   OurClasses = 'ourclasses',
+//   ContactUs = 'contactus'
+
+// }
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
+  return(
+  <div className="app bg-gray-20">
+    <Navbar 
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+    />
+
+  </div>
+  )
+}
+
+export default App
