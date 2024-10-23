fd2b96476a41f35010ce4932322f163543ff1668
Shahir Ahmed <ahmeds@dickinson.edu>
2024-10-23 05:15:46 -0400

    Benefits page done

--- fitness/src/scenes/benefits/index.tsx
+++ fitness/src/scenes/benefits/index.tsx
@@ -6,7 +6,9 @@ import {
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
 
 const benefits: Array<BenefitType> = [
   {
@@ -82,6 +84,68 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          <img
+            className="mx-auto"
+            alt="benefits-page-graphic"
+            src={BenefitsPageGraphic}
+          />
+          <div>
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>.
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                At Megan's Gym, we focus on helping our members achieve their
+                fitness goals through tailored workout plans, modern equipment,
+                and an encouraging community. Our certified trainers and
+                supportive environment ensure you stay on track, whether you're
+                just starting out or taking your fitness to the next level.
+              </p>
+              <p className="mb-5">
+                {" "}
+                Join the millions of happy members who have transformed their
+                lives with us. We offer personalized coaching, flexible class
+                schedules, and an array of fitness programs designed to fit your
+                lifestyle. At Megan's Gym, it's not just about working out—it's
+                about building lasting health and well-being.
+              </p>
+            </motion.div>
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
