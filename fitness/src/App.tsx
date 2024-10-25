328c2fff30b926d42ca2cce6344c197b562356f8
megantriplett <megantriplett10@gmail.com>
2024-10-25 15:24:50 -0400

    pls

--- fitness/src/App.tsx
+++ fitness/src/App.tsx
@@ -2,7 +2,9 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
-import ContactUs from "@/scenes/contactus";
+import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -44,7 +46,9 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
