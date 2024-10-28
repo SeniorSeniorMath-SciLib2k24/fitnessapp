10a7a9426c1fc87a743b927fd4e2f4ee54627477
Shahir Ahmed <ahmeds@dickinson.edu>
2024-10-28 04:37:36 -0400

    Add  component tests for ActionButton, HText, Footer, and Navbar components. also adds more rigorous testing for the contact us e2e tests

--- fitness/cypress/e2e/contactus.cy.ts
+++ fitness/cypress/e2e/contactus.cy.ts
@@ -1,80 +1,68 @@
-describe("template spec", () => {
+describe("Contact Us Page E2E Tests", () => {
   beforeEach(() => {
     cy.viewport(1280, 800);
     cy.visit("/");
     cy.contains("Join Now").click();
   });
 
-  it("Requires input", () => {
-    cy.get('[placeholder="NAME"]').should("be.empty");
-    cy.get('[placeholder="EMAIL"]').should("be.empty");
-    cy.get('[placeholder="MESSAGE"]').should("be.empty");
+  it("should display the correct header and description", () => {
+    cy.contains("JOIN NOW TO GET IN SHAPE").should("be.visible");
+    cy.contains(
+      "Send us an email to jumpstart your ethereal transformation. All it takes! One email!",
+    ).should("be.visible");
+  });
 
-    // cy.contains('This field is required').should('not.be.visible')
-    cy.get('button[type="submit"]').click();
+  it("should display the contact form", () => {
+    cy.get("form").should("exist");
+    cy.get('[placeholder="NAME"]').should("be.visible");
+    cy.get('[placeholder="EMAIL"]').should("be.visible");
+    cy.get('[placeholder="MESSAGE"]').should("be.visible");
+    cy.get("button[type='submit']")
+      .should("contain", "SUBMIT")
+      .should("be.visible");
+  });
 
-    // name
+  it("requires input for name, email, and message", () => {
+    cy.get("button[type='submit']").click();
+
     cy.get('[placeholder="NAME"]')
       .next("p")
       .should("be.visible")
       .and("contain.text", "This field is required.");
-
-    // email
     cy.get('[placeholder="EMAIL"]')
       .next("p")
       .should("be.visible")
       .and("contain.text", "This field is required.");
-
-    // message
     cy.get('[placeholder="MESSAGE"]')
       .next("p")
       .should("be.visible")
       .and("contain.text", "This field is required.");
   });
 
-  it("Sends Form Correctly", () => {
+  it("sends form correctly with valid inputs", () => {
     cy.get('[placeholder="NAME"]').type("Megan");
-    cy.get('[placeholder="NAME"]').should("have.value", "Megan");
-
     cy.get('[placeholder="EMAIL"]').type("fake@email.com");
-    cy.get('[placeholder="EMAIL"]').should("have.value", "fake@email.com");
-
     cy.get('[placeholder="MESSAGE"]').type("please send me the workout");
-    cy.get('[placeholder="MESSAGE"]').should(
-      "have.value",
-      "please send me the workout",
-    );
 
     cy.contains("SUBMIT").click();
+
+    cy.contains("This field is required.").should("not.exist");
+    cy.contains("Max length is 100 characters.").should("not.exist");
+    cy.contains("Invalid email address").should("not.exist");
   });
 
-  it("Error on max character length", () => {
-    cy.get('[placeholder="NAME"]').type(
-      "Meganhfhsjsdjdkkdjkjfkhjfhfdhfhdsjdskddjkjkdjdfjdjdjddjdjjdkjkdjdkjdjkdjddkjdkjdkjdjkdjkdjkdjkddkjdjkjdkkjdkjddsdhkhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
-    );
-    // cy.get('[placeholder="NAME"]').should('have.value', 'Megan')
-
-    cy.get('[placeholder="EMAIL"]').type("fake@email");
-    cy.get('[placeholder="EMAIL"]').should("have.value", "fake@email");
-
+  it("shows errors on invalid input", () => {
+    cy.get('[placeholder="NAME"]').type("M".repeat(101));
+    cy.get('[placeholder="EMAIL"]').type("invalid@email");
     cy.get('[placeholder="MESSAGE"]').type("please send me the workout");
-    cy.get('[placeholder="MESSAGE"]').should(
-      "have.value",
-      "please send me the workout",
-    );
 
     cy.contains("SUBMIT").click();
 
-    cy.contains("Max length is 100 characters.");
-    cy.contains("Invalid email address");
+    cy.contains("Max length is 100 characters.").should("be.visible");
+    cy.contains("Invalid email address").should("be.visible");
+  });
 
-    // cy.get('#form-validation').within(() => {
-    //   // at first both input elements are invalid
-    //   cy.get('input:invalid').should('have.length', 2)
-
-    //   cy.log('**enter the item**')
-    //   cy.get('#item').type('push pin')
-    //   cy.get('input:invalid').should('have.length', 1)
-    // })
+  it("displays the correct contact image", () => {
+    cy.get('img[alt="contact-us-page-graphic"]').should("be.visible");
   });
 });
